2b0660645a8e0910db95cef4d679482567d76848
jypweback <qkrwpdud1@gmail.com>
2020-10-01 23:58:55 +0900

    window

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+
+        public void PrintTest()
+        {
+            Console.WriteLine("PrintTest");
+        }
+    }
+}
